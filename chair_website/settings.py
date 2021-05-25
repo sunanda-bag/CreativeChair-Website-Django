10520405fad5923417011d4b03c7897943e03385
Sunanda <sunandabag63@gmail.com>
2021-05-25 20:59:49 +0530

    Added few changes

--- chair_website/settings.py
+++ chair_website/settings.py
@@ -155,6 +155,6 @@ django_heroku.settings(locals())
 # email 
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST', default='localhost')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='localhost')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=False, cast=bool)
