844e284c6207d3418d9df9bbb2c5643944de4ead
sunanda <sunandabag63@gmail.com>
2021-05-28 12:52:47 +0530

    heroku

--- chair_website/wsgi.py
+++ chair_website/wsgi.py
@@ -11,9 +11,10 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # from whitenoise.django import DjangoWhiteNoise
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chair_website.settings')
 
 application = get_wsgi_application()
 
-# application = DjangoWhiteNoise(application)
+application = WhiteNoise(application)
